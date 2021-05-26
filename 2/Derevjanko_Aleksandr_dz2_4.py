e79e0266e12173852ade8a23c3715dcfb3a1f79d
Aleksandr <derevjanko9@gmail.com>
2021-05-26 20:03:50 +0300

    Урок 2. Некоторые встроенные типы и операции с ними. ДЗ.

---
+++ 2/Derevjanko_Aleksandr_dz2_4.py
@@ -0,0 +1,9 @@
+list_1 = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+list_2 = list_1[0].split()
+print(f'Привет, {list_2[-1].title()}!')
+list_3 = list_1[1].split()
+print(f'Привет, {list_3[-1].title()}!')
+list_4 = list_1[2].split()
+print(f'Привет, {list_4[-1].title()}!')
+list_5 = list_1[3].split()
+print(f'Привет, {list_5[-1].title()}!')
